84fbecdd0a9c2d6bcde9058c1c88fa4e5f41e6a1
student <volzhenin.egor@gmail.com>
2026-04-07 14:47:05 +0300

    07.04.26

--- models/user_auth_service.py
+++ models/user_auth_service.py
@@ -3,4 +3,4 @@ from models.user import User
 class UserAuthService:
     @staticmethod
     def create_token(user):
-        return str(user.get_id() + ':' + user.get_auth_token())
+        return str(user.get_id()) + ':' + user.get_auth_token()
